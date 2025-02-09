6d19307625919b619f91f5edbbabc5722f20c1b9
Seyoon Wang <yoonsound79@gmail.com>
2025-02-09 15:09:58 +0900

    seo 작업

--- app/page.tsx
+++ app/page.tsx
@@ -24,8 +24,20 @@ const Home: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100">
       <Head>
-        <title>띵킹캣</title>
-        <meta name="띵킹캣" content="앱 ・ 프로그램 개발" />
+        <title>띵킹캣 - 앱개발 및 프로그램 개발 전문 기업</title>
+        <meta name="description" content="띵킹캣은 혁신적인 앱 개발과 프로그램 개발을 전문으로 하는 기업입니다. 소프트웨어를 통해 더 편리한 일상과 새로운 가치를 제공합니다." />
+        <meta name="keywords" content="띵킹캣, thinking cat, thinkingcat, 앱개발, 프로그램개발, 소프트웨어개발, 모바일앱, 웹개발" />
+        
+        {/* Open Graph 태그 추가 (소셜 미디어 공유용) */}
+        <meta property="og:title" content="띵킹캣 - 앱개발 및 프로그램 개발 전문 기업" />
+        <meta property="og:description" content="띵킹캣은 혁신적인 앱 개발과 프로그램 개발을 전문으로 하는 기업입니다." />
+        <meta property="og:type" content="website" />
+        <meta property="og:image" content="/thinkingcat.png" />
+        <meta property="og:url" content="https://www.thinkingcatworks.com/" />
+        
+        {/* 기타 메타 태그 */}
+        <meta name="robots" content="index,follow" />
+        <meta name="author" content="ThinkingCAT" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
